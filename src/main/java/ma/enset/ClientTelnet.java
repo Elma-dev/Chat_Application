d2f92a7b8b315be2baa962398925b2a6b77009b7
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-02-14 00:33:40 +0100

    second

--- src/main/java/ma/enset/ClientTelnet.java
+++ src/main/java/ma/enset/ClientTelnet.java
@@ -1,11 +1,86 @@
 package ma.enset;
 
+import com.sun.javafx.geom.Area;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.awt.*;
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ClientTelnet {
+public class ClientTelnet extends Application {
     public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    public void start(Stage stage) throws IOException {
+
+
+
+        AnchorPane root=new AnchorPane();
+
+        TextField message=new TextField();
+        message.setLayoutX(10);
+        message.setLayoutY(320);
+        message.setPrefWidth(500);
+        message.setPrefHeight(30);
+
+        Button send=new Button("Send");
+        send.setLayoutX(515);
+        send.setLayoutY(320);
+
+
+        ScrollPane msgPane=new ScrollPane();
+        msgPane.setPrefWidth(515);
+        msgPane.setPrefHeight(280);
+        msgPane.setLayoutX(10);
+        msgPane.setLayoutY(35);
+
+        Text to=new Text("To :");
+        to.setX(10);
+        to.setY(13);
+
+        TextField msgTo=new TextField();
+        msgTo.setPrefWidth(490);
+        msgTo.setLayoutX(35);
+        msgTo.setLayoutY(0);
+
+
+
+
+        AnchorPane contentPane=new AnchorPane();
+        contentPane.setStyle("-fx-background-color: WHITE");
+        contentPane.setLayoutX(0);
+        contentPane.setLayoutY(0);
+        contentPane.setPrefWidth(510);
+        contentPane.setPrefHeight(280);
+        msgPane.setContent(contentPane);
+
+
+        root.getChildren().add(message);
+        root.getChildren().add(send);
+        root.getChildren().add(to);
+        root.getChildren().add(msgTo);
+        root.getChildren().add(msgPane);
+
+
+
+
+
         Socket socket=new Socket("localhost",123);
         InputStream is=socket.getInputStream();
         OutputStream os=socket.getOutputStream();
@@ -15,22 +90,74 @@ public class ClientTelnet {
 
         PrintWriter printWriter=new PrintWriter(os,true);
 
-
         new Thread(()->{
             String serverMsg;
             try{
-            while((serverMsg=br.readLine())!=null){
-                System.out.println(serverMsg);
-            }
+                while((serverMsg=br.readLine())!=null){
+                    System.out.println("Here "+serverMsg);
+
+
+                    double y=10;
+                    if(contentPane.getChildren().size()-1>=0) {
+                        y = contentPane.getChildren().get(contentPane.getChildren().size() - 1).getLayoutY()+20;
+                    }
+
+
+                    Text test=new Text(serverMsg);
+
+
+                    test.setFill(Color.RED);
+                    test.setLayoutX(10);
+                    test.setLayoutY(y);
+                    Platform.runLater(()->{
+                        contentPane.getChildren().add(test);
+                    });
+
+
+                }
             }catch (Exception e){
             }
         }).start();
 
 
-        while(true){
-            String msgToSrv=new Scanner(System.in).nextLine();
-            printWriter.println(msgToSrv);
-        }
+        send.setOnAction(actionEvent -> {
+            double y=10;
+            if(contentPane.getChildren().size()-1>=0) {
+                y = contentPane.getChildren().get(contentPane.getChildren().size() - 1).getLayoutY()+20;
+            }
+
+
+            Text test=new Text("me: "+message.getText());
+            test.setFill(Color.GREEN);
+            test.setLayoutX(10);
+            test.setLayoutY(y);
+
+            contentPane.getChildren().add(test);
+            if(!msgTo.getText().isEmpty()){
+                System.out.println(msgTo.getText()+"=>"+message.getText());
+                printWriter.println(msgTo.getText()+"=>"+message.getText());
+            }
+            else{
+                printWriter.println(message.getText());
+            }
+
+
+
+
+            message.setText("");
+
+        });
+
+
+
+
+
+        Scene scene=new Scene(root);
+        stage.setScene(scene);
+        stage.setHeight(400);
+        stage.setWidth(600);
+        stage.setTitle("ClientChat");
+        stage.show();
 
     }
 }
--- src/main/java/ma/enset/MTServer.java
+++ src/main/java/ma/enset/MTServer.java
@@ -1,25 +1,38 @@
 package ma.enset;
 
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
-public class MTServer extends Thread{
+public class MTServer implements Runnable {
+
+    VBox boxClients=new VBox();
+    static ArrayList<Socket> clients=new ArrayList<>();
     public static void main(String[] args) {
-        new MTServer().start();
+        new Thread(new MTServer()).start();
+
     }
 
     @Override
     public void run() {
         int clientId=0;
-        ArrayList<Socket> clients=new ArrayList<>();
+        //ArrayList<Socket> clients=new ArrayList<>();
         try {
             ServerSocket ss=new ServerSocket(123);
             Socket client;
-            InputStream is;
-            OutputStream os;
 
             while (true){
                 client=ss.accept();
@@ -27,17 +40,15 @@ public class MTServer extends Thread{
                 new Conversation(client,++clientId,clients).start();
                 System.out.println("Client "+clientId+" Joined Conversation IP="+client.getRemoteSocketAddress());
 
-
             }
-
-
-
 
         }catch (Exception e){
 
         }
 
     }
+
+
 
     public class Conversation extends Thread{
         private Socket client;
@@ -67,16 +78,39 @@ public class MTServer extends Thread{
 
                 while ((msgClient= br.readLine())!=null){
                     System.out.println("client "+this.clientId+": "+msgClient);
-                    for(Socket c : clients){
-                        if(c!=client) {
-                            pw = new PrintWriter(c.getOutputStream(), true);
-                            pw.println("client" + clientId + ": " + msgClient);
+                    if(msgClient.contains("=>")){
+                        String to= (msgClient.split("=>"))[0];
+                        msgClient= (msgClient.split("=>"))[1];
+                        if(to.contains(",")){
+                            String[] msgTo=to.split(",");
+                            for(String x : msgTo){
+                                int index=Integer.parseInt(x);
+                                if(clients.size()>=index) {
+                                    pw = new PrintWriter(((Socket) clients.get(index-1)).getOutputStream(), true);
+                                    pw.println("client" + clientId + ": " + msgClient);
+                                }
+                            }
+                        }else{
+                            int index=Integer.parseInt(to);
+                            if(clients.size()>=index) {
+                                pw = new PrintWriter(((Socket) clients.get(index-1)).getOutputStream(), true);
+                                pw.println("client" + clientId + ": " + msgClient);
+                            }
+                        }
+
+                    }else {
+                        for (Socket c : clients) {
+                            if (c != client) {
+                                pw = new PrintWriter(c.getOutputStream(), true);
+                                pw.println("client" + clientId + ": " + msgClient);
+                            }
                         }
                     }
 
                 }
 
             }catch (Exception e){
+                e.printStackTrace();
 
             }
 
